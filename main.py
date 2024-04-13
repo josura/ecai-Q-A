6b9421ff7db63c2abc2cd18bd03c4ae9c05c5c1f
josura <giorgiolocicero@hotmail.com>
2024-04-13 13:33:02 +0200

    almost finished the code to read docs and questions from text and ask questions to gpt model, needs to be tested

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain.document_loaders import TextLoader
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_openai import ChatOpenAI
+import utilities
 
 
 load_dotenv(find_dotenv())
@@ -18,13 +19,92 @@ model_id = "gpt-3.5-turbo"
 
 OpenAI_key = os.environ.get("OPEN_AI_KEY")
 
-# Define the LLM we plan to use. Here we are going to use ChatGPT 3.5 turbo
-llm=ChatOpenAI(model_name = model_id, temperature=0.2, max_tokens=100, api_key=OpenAI_key)
 
-# Define the document loader
+
+# Document loader
 document_loader = TextLoader()
 # pdf loader
 pdf_loader = PyPDFLoader()
 
-# Define the index creator
-index_creator = VectorstoreIndexCreator()
+
+textFolder = 'data/text'
+questionsFolder = 'data/questions'
+answersFolder = 'results/answers'
+
+max_iterations = 10
+
+# for every text file in the text folder, search for the corresponding questions file in the questions folder and ask the questions
+docs = []
+pdfs = []
+
+docs = utilities.list_txt_files(textFolder)
+pdfs = utilities.list_pdf_files(textFolder)
+
+for i in range(max_iterations): 
+    for doc in docs:
+        full_path = os.path.join(textFolder, doc)
+        # Define the LLM we plan to use. Here we are going to use ChatGPT 3.5 turbo
+        current_temperature = 0.2
+        current_max_tokens = 100
+        llm=ChatOpenAI(model_name = model_id, temperature=current_temperature, max_tokens=current_max_tokens, api_key=OpenAI_key)
+
+        # Get the list of questions (multiple answers of questions)
+        # the questions are organized in the file with the following format:
+        # <number>. <question>
+        # <A, B, C, D> answers
+
+        questions = utilities.read_questions(os.path.join(questionsFolder, doc))
+
+        # Load the document
+        document = document_loader.load_document(full_path)
+        # Define the index creator
+        index_creator = VectorstoreIndexCreator()
+        # Add the document to the index
+        index_creator.add_document(document)
+
+        for question in questions:
+            print("Question: ", question)
+            answers = llm.ask_question(question, index)
+            print("Answers: ", answers)
+            print("\n\n")
+            # save the answers in a csv file where the format is the following:
+            # <question>, <answer>, <document>, <temperature>, <max_tokens>, <iterations>
+            file_name = doc.split(".")[0]
+            output = questionsFolder + '/' + file_name + '.csv'
+            with open(output, 'a') as f:
+                f.write(question + ', ' + answers + ', ' + doc + ', ' + str(current_temperature) + ', ' + str(current_max_tokens) + ', ' + str(i) + '\n')
+
+
+for i in range(max_iterations):
+    for pdf in pdfs:
+        full_path = os.path.join(textFolder, pdf)
+        # Define the LLM we plan to use. Here we are going to use ChatGPT 3.5 turbo
+        current_temperature = 0.2
+        current_max_tokens = 100
+        llm=ChatOpenAI(model_name = model_id, temperature=current_temperature, max_tokens=current_max_tokens, api_key=OpenAI_key)
+
+        # Get the list of questions (multiple answers of questions)
+        # the questions are organized in the file with the following format:
+        # <number>. <question>
+        # <A, B, C, D> answers
+
+        questions = utilities.read_questions(os.path.join(questionsFolder, pdf))
+
+        # Load the document
+        document = pdf_loader.load_document(full_path)
+        # Define the index creator
+        index_creator = VectorstoreIndexCreator()
+        # Add the document to the index
+        index_creator.add_document(document)
+
+        for question in questions:
+            print("Question: ", question)
+            answers = llm.ask_question(question, index)
+            print("Answers: ", answers)
+            print("\n\n")
+            # save the answers in a csv file where the format is the following:
+            # <question>, <answer>, <document>, <temperature>, <max_tokens>, <iterations>
+            file_name = pdf.split(".")[0]
+            output = questionsFolder + '/' + file_name + '.csv'
+            with open(output, 'a') as f:
+                f.write(question + ', ' + answers + ', ' + pdf + ', ' + str(current_temperature) + ', ' + str(current_max_tokens) + ', ' + str(i) + '\n')
---
+++ utilities.py
@@ -0,0 +1,53 @@
+import os
+
+def read_questions(file_path):
+    questions = []
+    # control if the file exists
+    if not os.path.exists(file_path):
+        print(f"File {file_path} does not exist")
+        return questions
+    print(f"Reading questions from {file_path}")
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        current_line = 0
+        while current_line < len(lines):
+            # if the line starts with a number followed by a dot, then it is a question. multiple answers questions are defined by the following format:
+            # <number>. <question>
+            # <A, B, C, D> answers
+            # there could also be free answers questions
+            current_line_strip = lines[current_line].split(".")# use the dot to split the number from the question
+            if current_line_strip[0].isdigit():
+                # get the question
+                question = lines[current_line]
+                # control if the question is a multiple answers question or a free answer question
+                if current_line + 1 < len(lines) and not lines[current_line + 1][0].isdigit():
+                    # get the 4 answers, append them to the question string
+                    answers = []
+                    for i in range(4):
+                        answers.append(lines[current_line + 1 + i])
+                    question += " ".join(answers)
+                    questions.append(question)
+                    current_line += 5
+                else:
+                    questions.append(question)
+                    current_line += 1
+            
+    return questions
+
+def list_txt_files(folder):
+    # list all the text files in the folder
+    files = os.listdir(folder)
+    txt_files = []
+    for file in files:
+        if file.endswith(".txt"):
+            txt_files.append(file)
+    return txt_files
+
+def list_pdf_files(folder):
+    # list all the pdf files in the folder
+    files = os.listdir(folder)
+    pdf_files = []
+    for file in files:
+        if file.endswith(".pdf"):
+            pdf_files.append(file)
+    return pdf_files
